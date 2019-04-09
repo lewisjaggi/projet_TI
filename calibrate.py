3144adc5ed00ee6c7b64764a76e35c63ec3fb9ac
Steven Jeanneret <steven.jeanneret@he-arc.ch>
2019-04-09 12:42:25 +0200

    dist pixel in function

--- calibrate.py
+++ calibrate.py
@@ -32,47 +32,45 @@ def find_chess(img):
     # If found, add object points, image points (after refining them)
     if ret:
         objpoints.append(objp)
-
         cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
+
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    h, w = img.shape[:2]
-    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-    mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
-    calibrated = True
-    print("Calibration Ok")
-    print("-------------------------------------------------")
-    print("mtx : ", mtx)
-    print("dist : ", dist)
     return ret, corners, mtx, dist
 
 
-if __name__ == '__main__':
-    frame = get_first_frame('test/film/test1.mp4')
-    ret, corners, mtx, dist = find_chess(frame)
-    print(f"corners {type(mtx)}")
+def get_dist_pixel(corners):
     p1 = corners[0]
     p2 = corners[chessH - 1]
     p3 = corners[-1]
     p4 = corners[-chessH]
-    print(f"p1 : {p1 } p2 : {p2 } p3 : {p3 } p4 :  {p4} ")
-    height_real_pixel = (p1 - p2)[0][1]
-    print(f"height_real_pixel : {height_real_pixel}")
-    width_real_pixel = (p3 - p2)[0][0]
-    print(f"width_real_pixel : {width_real_pixel}")
+    height_real_pixel_1 = abs((p1 - p2)[0][1])
+    width_real_pixel_1 = abs((p3 - p2)[0][0])
 
-    height_real_pixel = (p4 - p3)[0][1]
-    print(f"height_real_pixel : {height_real_pixel}")
-    width_real_pixel = (p4 - p1)[0][0]
-    print(f"width_real_pixel : {width_real_pixel}")
+    diag1 = ((p1[0][0] - p3[0][0]) ** 2 + (p1[0][1] - p3[0][1]) ** 2) ** 0.5
+    diag2 = ((p2[0][0] - p4[0][0]) ** 2 + (p2[0][1] - p4[0][1]) ** 2) ** 0.5
+    diag_real = (((chessH - 1) * square_height) ** 2 + ((chessW - 1) * square_width) ** 2) ** 0.5
+
+    height_real_pixel_2 = abs((p4 - p3)[0][1])
+    width_real_pixel_2 = abs((p4 - p1)[0][0])
+
+    l_pixel_by_cm = [diag1 / diag_real, diag2 / diag_real, width_real_pixel_1 / ((chessW - 1) * square_width),
+                     width_real_pixel_2 / ((chessW - 1) * square_width),
+                     height_real_pixel_1 / ((chessH - 1) * square_width),
+                     height_real_pixel_2 / ((chessH - 1) * square_width)]
+
+    return sum(l_pixel_by_cm) / len(l_pixel_by_cm)
+
+
+if __name__ == '__main__':
+    frame = get_first_frame('test/film/test2_1.mp4')
+    ret, corners, mtx, dist = find_chess(frame)
+    dist_pixel = get_dist_pixel(corners)
     cv.drawChessboardCorners(frame, (chessH, chessW), corners, 1)
     h, w = frame.shape[:2]
     newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
     dst = cv.undistort(frame, mtx, dist, None)
     x, y, w, h = roi
     dst = dst[y:y + h, x:x + w]
-
-    pixel_by_cm = width_real_pixel/((chessW - 1)*square_width)
-    print(f"pixel by cm  : {pixel_by_cm}")
     cv.imshow('camera', dst)
     cv.waitKey(0)
